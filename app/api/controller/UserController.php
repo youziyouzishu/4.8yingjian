3b68d0aaf752edf6a6a6e51a40b9bca967fd4cf2
阿沁 <youziyouzishu@gmail.com>
2025-05-15 11:19:57 +0800

    feat(api): 添加获取个人信息接口- 新增 getUserInfo 方法，用于获取用户个人信息 - 使用 User模型查询数据库，根据用户 ID 获取信息 - 返回成功响应，包含用户信息数据

--- app/api/controller/UserController.php
+++ app/api/controller/UserController.php
@@ -2,11 +2,22 @@
 
 namespace app\api\controller;
 
+use app\admin\model\User;
 use app\api\basic\Base;
 use support\Request;
 
 class UserController extends Base
 {
 
+    /**
+     * 获取个人信息
+     * @param Request $request
+     * @return \support\Response
+     */
+    function getUserInfo(Request $request)
+    {
+        $row = User::find($request->user_id);
+        return $this->success('成功', $row);
+    }
 
 }
